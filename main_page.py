6d5fddab290fc430eaf8ecb1874b6c5d55275821
Tianshu Chu <tianshuchu@Tianshus-MacBook-Pro.local>
2023-09-10 23:46:27 -0700

    Remove unused buttons

--- main_page.py
+++ main_page.py
@@ -7,7 +7,7 @@ class MainPage:
 
     def __init__(self):
         self.root = tk.Tk('')
-        self.root.geometry("600x600")
+        self.root.geometry("400x200")
         self.label_person1 = tk.Label(text='Person 1')
         self.label_person1.grid(column=0, row=0)
         self.entry_person1 = tk.Entry(self.root)
@@ -16,14 +16,8 @@ class MainPage:
         self.label_person2.grid(column=0, row=1)
         self.entry_person2 = tk.Entry(self.root)
         self.entry_person2.grid(column=1, row=1)
-        self.label_depth = tk.Label(text='Search depth')
-        self.label_depth.grid(column=0, row=2)
-        self.entry_depth = tk.Entry(self.root)
-        self.entry_depth.grid(column=1, row=2)
-        self.result = tk.Text(self.root)
-        self.result.grid(column=0, row=3, columnspan=2)
         self.submit_button = tk.Button(self.root, text="submit", command=self.evaluate)
-        self.submit_button.grid(column=0, row=4, columnspan=2)
+        self.submit_button.grid(column=0, row=2, columnspan=2)
 
         self.root.mainloop()
 
